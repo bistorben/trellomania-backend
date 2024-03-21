1f6458797cec096f24cb32b95d937cfbbc29cf3c
Torben Bis <bis.torben@gmail.com>
2024-03-21 09:43:32 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,7 +86,7 @@ const loginUser = async (req, res, next) => {
       path: "/",
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "None",
+      sameSite: "Lax",
       maxAge: 1000 * 60 * 60,
     });
 
